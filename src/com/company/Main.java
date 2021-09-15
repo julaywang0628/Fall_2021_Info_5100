18a01c9dc3243f058003aac6a65137c5da7d8b2c
julay <julaywang0628@gmail.com>
2021-09-14 21:19:22 -0700

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        short max = Short.MAX_VALUE;
+        short min = Short.MIN_VALUE;
+
+        System.out.println("Max is " + max);
+        System.out.println("Min is " +min);
+
+
+
+
+    }
+
+}
